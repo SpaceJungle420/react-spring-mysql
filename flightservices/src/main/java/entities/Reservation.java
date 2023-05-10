9cf269b2174ebf43127f4d526df1c76bd89042a3
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-05-10 14:50:02 -0300

    define relationships

--- flightservices/src/main/java/entities/Reservation.java
+++ flightservices/src/main/java/entities/Reservation.java
@@ -1,15 +1,36 @@
 package entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Reservation extends AbstractEntity {
 
     private boolean checkIn;
     private int numberOfBags;
+    @OneToOne
+    private Flight flight;
+    @OneToOne
+    private Passenger passenger;
 
     public boolean isCheckIn() {
         return checkIn;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
     }
 
     public void setCheckIn(boolean checkIn) {
