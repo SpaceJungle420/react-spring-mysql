4e60fa42574e68598f132ed8a2d487267f77521b
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-05-16 08:27:39 -0300

    add browser router and routes

--- flightreservation/src/App.tsx
+++ flightreservation/src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import FindFlight from "./components/FindFlights";
+import DisplayFlights from "./components/DisplayFlights";
+import PassengerDetails from "./components/PassengerDetails";
+import ConfirmReservation from "./components/ConfirmReservation";
 
 function App() {
   return (
     <div>
       <h1>Flight Reservation App</h1>
+      <Routes>
+        <Route path="/" element={<FindFlight />} />
+        <Route path="/displayFlights" element={<DisplayFlights />} />
+        <Route path="/passengerDetails" element={<PassengerDetails />} />
+        <Route path="/confirmResorvation" element={<ConfirmReservation />} />
+      </Routes>
     </div>
   );
 }
