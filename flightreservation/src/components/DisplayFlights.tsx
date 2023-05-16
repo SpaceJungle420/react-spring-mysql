022bdb3cb3c15ca4668845506ce71019035fcf71
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-05-16 08:55:53 -0300

    add states

--- flightreservation/src/components/DisplayFlights.tsx
+++ flightreservation/src/components/DisplayFlights.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 function DisplayFlights() {
   const { to, from, departureCity } = useParams();
-  const [flightData, setFlightData] = useState();
+  const [flightData, setFlightData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  let count = 0;
+
   return (
     <div>
       <h1>Display Flights</h1>
