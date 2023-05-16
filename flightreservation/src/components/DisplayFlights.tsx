9327356a9b142cbbb5933b75dabaed85b528aba2
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-05-16 09:04:15 -0300

    add axio call to fetch flight data

--- flightreservation/src/components/DisplayFlights.tsx
+++ flightreservation/src/components/DisplayFlights.tsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function DisplayFlights() {
-  const { to, from, departureCity } = useParams();
+  const { to, from, departureDate } = useParams();
   const [flightData, setFlightData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   let count = 0;
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:8080/flightservices/flights?from=" +
+          from +
+          "&to=" +
+          to +
+          "&departureDate=" +
+          departureDate
+      )
+      .then((res) => {
+        setFlightData(res.data);
+        setIsLoading(false);
+      });
+  }, [count]);
 
   return (
     <div>
