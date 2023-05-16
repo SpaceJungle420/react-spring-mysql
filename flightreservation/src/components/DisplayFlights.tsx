081fa76eb8ad9d7e7960b790a0c946d69e0fdd73
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-05-16 16:55:11 -0300

    update to display list of flights

--- flightreservation/src/components/DisplayFlights.tsx
+++ flightreservation/src/components/DisplayFlights.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function DisplayFlights() {
-  const { to, from, departureDate } = useParams();
+  const { from, to, departureDate } = useParams();
   const [flightData, setFlightData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   let count = 0;
@@ -20,13 +20,51 @@ function DisplayFlights() {
       )
       .then((res) => {
         setFlightData(res.data);
+        console.log(
+          "http://localhost:8080/flightservices/flights?from=" +
+            from +
+            "&to=" +
+            to +
+            "&departureDate=" +
+            departureDate
+        );
+        console.log(res.data);
         setIsLoading(false);
       });
   }, [count]);
 
+  function RowCreator(props: any) {
+    var flight = props.item;
+
+    return (
+      <tr>
+        <td>{flight.operatingAirlines}</td>
+        <td>{flight.departureCity}</td>
+        <td>{flight.arrivalCity}</td>
+        <td>{flight.estimatedDepartureTime}</td>
+        <td>
+          <Link to={"/passengerDetails/" + flight.id}>Select</Link>
+        </td>
+      </tr>
+    );
+  }
+
   return (
     <div>
       <h1>Display Flights</h1>
+      <table>
+        <thead>
+          <th>Airlines</th>
+          <th>Departure City</th>
+          <th>Arrival City</th>
+          <th>Departure Date and Time</th>
+        </thead>
+        <tbody>
+          {!isLoading
+            ? flightData.map((flight) => <RowCreator item={flight} />)
+            : ""}
+        </tbody>
+      </table>
     </div>
   );
 }
